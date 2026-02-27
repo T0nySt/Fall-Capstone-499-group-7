15a5ed93901724295a3ee8f66d2b36890472bdf7
Matthew Wen <you@example.com>
2026-02-27 17:13:52 -0500

    Revert "Revert "Revert "set up backend connection to the cloud instead of locally"""
    This reverts commit 497402963dfbadae36a11109966427eebaed18b8.

--- hun-show/backend/src/app.module.ts
+++ hun-show/backend/src/app.module.ts
@@ -1,44 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [
-    // Loads .env globally
-    ConfigModule.forRoot({ isGlobal: true }),
-
-    // Redis placeholder
-    RedisModule,
-
-    // PostgreSQL, for users, sessions, watchparty metadata, playback progress
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('POSTGRES_HOST'),
-        port: config.get<number>('POSTGRES_PORT'),
-        username: config.get<string>('POSTGRES_USER'),
-        password: config.get<string>('POSTGRES_PASSWORD'),
-        database: config.get<string>('POSTGRES_DB'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true, // Auto-creates tables from entities, REMEMBER TO SET TO FALSE IN WHEN IN PROD
-      }),
-      inject: [ConfigService],
-    }),
-
-    // MongoDB, for video metadata, genres, subtitles
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        uri: config.get<string>('MONGO_URI'),
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
